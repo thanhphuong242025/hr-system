ccf8c11660857f16bcbea0d718a7d8c6af425b9c
Nguyễn Thanh Phương <phuongg6324@gmail.com>
2026-04-10 15:08:53 +0700

    fix: Express v5 wildcard route syntax

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all: serve React app for any non-API route
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
